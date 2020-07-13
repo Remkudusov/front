38ad3f17e2d8b38a48c978619bb8808034fb27da
Rem <rem.kudusov@gmail.com>
2020-07-14 00:40:44 +0300

    hiding secret key

--- MadCRM/settings.py
+++ MadCRM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iii*2^fh@-9mu9baub2%%%+04w17-gmzb04g$ee1-3cdmh&ftb'
+SECRET_KEY = 'key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
