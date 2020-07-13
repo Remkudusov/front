309c994fe97d0cc3ef90f7994918713e2ddca1d7
Rem <rem.kudusov@gmail.com>
2020-07-13 15:16:20 +0300

    Add files via upload

---
+++ static/login/index.js
@@ -0,0 +1,9 @@
+function openRegistration() {
+    document.getElementById("enter").style.display = "none";
+    document.getElementById("registration").style.display = "inline-block";
+}
+
+function openEnter() {
+    document.getElementById("registration").style.display = "none";
+    document.getElementById("enter").style.display = "inline-block";
+}
